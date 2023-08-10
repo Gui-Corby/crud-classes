ff8a8a78beec5e7560971422c151c8b01fe45472
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-08-10 12:21:17 -0300

    fix: missing console.log

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://blog-fake-api.onrender.com",
-    timeout: 8 * 1000,
-})
+   baseURL: "https://blog-fake-api.onrender.com",
+   timeout: 8 * 1000,
+});
+
+const token = localStorage.getItem("@TOKEN");
